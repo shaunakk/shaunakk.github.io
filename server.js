d95d8ff563719f05dc8d735f53975f087914081b
Shaunak Kale <epicshaunak@gmail.com>
2017-05-06 15:06:50 -0700

    start

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('ECHOLESS \n\n Made at AngelHack SF 2017')
 })
 
 app.listen(app.get('port'), function() {
